0223e7f4f651e40de3cec38b7fbb6e0a0daee9a9
鼠笑天 <zhuchuangang20@163.com>
2017-07-03 19:56:24 +0800

    每个reactor维护一个连接池

--- src/main/java/com/szss/mysqlproxy/backend/BackendConnectionPool.java
+++ src/main/java/com/szss/mysqlproxy/backend/BackendConnectionPool.java
@@ -13,41 +13,43 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class BackendConnectionPool {
 
-  private static Logger logger = LogManager.getLogger(BackendConnectionPool.class);
+    private static Logger logger = LogManager.getLogger(BackendConnectionPool.class);
 
-  private static BackendConnectionPool pool;
-  private ConcurrentHashMap<String, List<BackendConnection>> conMap;
+    private static ThreadLocal<BackendConnectionPool> backendConnectionPools = new ThreadLocal<>();
+    private ConcurrentHashMap<String, List<BackendConnection>> conMap;
 
-  private BackendConnectionPool() {
-    conMap = new ConcurrentHashMap();
-  }
-
-  public static BackendConnectionPool getInstance() {
-    if (pool == null) {
-      pool = new BackendConnectionPool();
+    private BackendConnectionPool() {
+        conMap = new ConcurrentHashMap();
     }
-    return pool;
-  }
 
-  public void addConnection(BackendConnection connection) {
-    List<BackendConnection> backendCons = conMap.get(connection.getReactorName());
-    if (backendCons == null) {
-      backendCons = new LinkedList<>();
+    public static BackendConnectionPool getInstance() {
+        BackendConnectionPool pool = backendConnectionPools.get();
+        if (pool == null) {
+            pool = new BackendConnectionPool();
+            backendConnectionPools.set(pool);
+        }
+        return pool;
     }
-    backendCons.add(connection);
-    this.conMap.put(connection.getReactorName(), backendCons);
-  }
 
-  public BackendConnection connection(String reactorName) throws IOException {
-    BackendConnection con = null;
-    List<BackendConnection> backendCons = conMap.get(reactorName);
-    if (backendCons!=null&&!backendCons.isEmpty()) {
-      con = backendCons.remove(0);
+    public void addConnection(BackendConnection connection) {
+        List<BackendConnection> backendCons = conMap.get(connection.getReactorName());
+        if (backendCons == null) {
+            backendCons = new LinkedList<>();
+        }
+        backendCons.add(connection);
+        this.conMap.put(connection.getReactorName(), backendCons);
     }
-    if (con == null) {
-      con = BackendConnectionFactory.make(reactorName);
+
+    public BackendConnection connection(String reactorName) throws IOException {
+        BackendConnection con = null;
+        List<BackendConnection> backendCons = conMap.get(reactorName);
+        if (backendCons != null && !backendCons.isEmpty()) {
+            con = backendCons.remove(0);
+        }
+        if (con == null) {
+            con = BackendConnectionFactory.make(reactorName);
+        }
+        return con;
     }
-    return con;
-  }
 
 }
